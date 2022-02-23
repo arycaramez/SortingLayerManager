246d82469cb3a3a031a1a561be0c93ca75f7e5e2
AryGuilhermePiresCaramez <agpc.dev@gmail.com>
2022-02-23 11:44:29 -0300

    Teste commit Git LFS

---
+++ SortingLayerCharManager/Assets/ScreenshotScript/Screenshot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour
+{
+    [SerializeField]
+    private string path;
+    [SerializeField]
+    [Range(1, 5)]
+    private int size = 1;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K)) {
+            path += "screenshot";
+            path += System.Guid.NewGuid().ToString() + ".png";
+            ScreenCapture.CaptureScreenshot(path, size);
+        }
+    }
+}
